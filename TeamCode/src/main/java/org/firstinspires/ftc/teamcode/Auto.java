5162f9186a14ddb6ff2056d1169ee2630de77300
ErrorCode23246 <149131927+ErrorCode23246@users.noreply.github.com>
2024-11-06 20:30:06 -0800

    Made a test for Auto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -5,26 +5,14 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous(name="Auto", group="Robot")
 public class Auto extends LinearOpMode {
-    private Hardware robot = new Hardware(this);
-    private ElapsedTime Runtime = new ElapsedTime();
+    Hardware robot = new Hardware(this);
+
     @Override
     public void runOpMode(){
         // Initialize all the hardware, using the hardware class.
         robot.init();
-
-        // Send telemetry message to signify robot waiting;
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        // Wait for the game to start (driver presses START)
         waitForStart();
-        Runtime.reset();
-        // Run until the end of the match (driver presses STOP)
-        robot.driveRobot(0, 0,0.5);
-        while (opModeIsActive() && (Runtime.seconds() < 3.0)) {
-            telemetry.addData("Path", "Leg 3: %4.1f S Elapsed", Runtime.seconds());
-            telemetry.update();
-        }
-        robot.driveRobot(0,0,0);
+        robot.AutoStrafe(24);
+
     }
 }
