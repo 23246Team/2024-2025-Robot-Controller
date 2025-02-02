c2e5be713139b8a2b859610677b332a23c2a77e6
ErrorCode23246 <149131927+ErrorCode23246@users.noreply.github.com>
2025-02-01 16:18:29 -0800

    Officialy on the left side can park

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftLong.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftLong.java
@@ -2,8 +2,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name="AutoFar", group="Test")
-public class AutoFar extends LinearOpMode {
+@Autonomous(name="LeftLong", group="Left")
+public class LeftLong extends LinearOpMode {
     Hardware robot = new Hardware(this);
 
     @Override
@@ -15,7 +15,21 @@ public class AutoFar extends LinearOpMode {
         telemetry.update();
 
         // Autonomous drive
-        robot.AutoStrafe(-70, 0);
+        robot.AutoDrive(-1.5,180);
+        robot.AutoStrafe(-20,180);
+        robot.AutoDrive(-47.3,180);
+        robot.AutoStrafe(-8.5,180);
+        robot.AutoDrive(51.1,180);
+        robot.AutoStrafe(-8.5,180);
+        robot.AutoDrive(-19.5,180,0.3);
+        robot.AutoStrafe(13,180);
+        robot.AutoDrive(-24.3,180);
+        robot.AutoStrafe(-10,180);
+        robot.AutoDrive(47,180);
+        robot.AutoDrive(-16,180);
+        robot.AutoStrafe(90.0,180,1, 0.5);
+        robot.AutoDrive(15,180);
+
 
 
         // Add telemetry to show final heading after drive
